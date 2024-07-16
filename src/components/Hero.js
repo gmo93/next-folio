01644c19345bb8add6703d0fbb43b54b2dc74a2e
gmo93 <omohundrogrant@yahoo.com>
2024-07-15 21:29:50 -0500

    working on hero interaction

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,32 @@
 import { useEffect, useState } from "react";
 import styles from "@/styles/Home.module.css";
+import Image from "next/image";
 
 export default function Hero() {
   const [count, setCount] = useState(0);
-  const wordArr = ["Brainstorm", "Blueprint", "Cry", "Buildout"];
+  const [rotate, setRotate] = useState("0");
+  const [scrollPos, setScrollPos] = useState(0);
+
+  const wordArr = [
+    "Brainstorm",
+    "Blueprint",
+    "Buildout",
+    "Believe",
+    "Troubleshoot",
+    "Problem Solve",
+    "Ponder",
+    "Create",
+    "Envision",
+    "Learn",
+  ];
   const [word, setWord] = useState(wordArr[0]);
+
+  function handleScroll() {
+    const pos = window.scrollY;
+    setScrollPos(pos);
+    console.log(window.scrollY);
+  }
+
   useEffect(() => {
     setTimeout(() => {
       setCount((count) => (count += 1));
@@ -12,10 +34,65 @@ export default function Hero() {
     }, "2000");
   }, [count]);
   return (
-    <div className={styles.hero}>
-      <p id="bstorm" className={styles.heroTextLeft}>
-        {word}
-      </p>
+    <div onClick={handleScroll} className={styles.hero}>
+      <div
+        className={styles.heroImgHolder}
+        onMouseOver={() => {
+          setRotate("150%");
+        }}
+        onMouseLeave={() => {
+          setRotate("0%");
+        }}
+      >
+        <span
+          style={{
+            transform: `translateX(${rotate})`,
+            transition: "transform 1s ease-in-out",
+          }}
+          className={styles.outHero}
+          id="lineOne"
+        ></span>
+        <span
+          style={{
+            transform: `translateX(-${rotate})`,
+            transition: "transform 1s ease-in-out",
+          }}
+          className={styles.oneInHero}
+          id="lineTwo"
+        ></span>
+        <span
+          style={{
+            transform: `translateX(${rotate})`,
+            transition: "transform 1s ease-in-out",
+          }}
+          className={styles.centerHero}
+          id="lineFour"
+        ></span>
+        <span
+          style={{
+            transform: `translateX(-${rotate})`,
+            transition: "transform 1s ease-in-out",
+          }}
+          className={styles.centerHero}
+          id="lineFour"
+        ></span>
+        <span
+          style={{
+            transform: `translateX(${rotate})`,
+            transition: "transform 1s ease-in-out",
+          }}
+          className={styles.oneInHero}
+          id="lineFive"
+        ></span>
+        <span
+          style={{
+            transform: `translateX(-${rotate})`,
+            transition: "transform 1s ease-in-out",
+          }}
+          className={styles.outHero}
+          id="lineSix"
+        ></span>
+      </div>
     </div>
   );
 }
