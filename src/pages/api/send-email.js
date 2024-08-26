33fc7b2659fea899d47ddbbc537fc67caa6092dd
gmo93 <omohundrogrant@yahoo.com>
2024-08-26 18:55:36 -0500

    Started the contact us section. Put a form in place, with refs attached to each section, to eventually change the state of the email request being sent out

---
+++ src/pages/api/send-email.js
@@ -0,0 +1,17 @@
+import { sendEmail } from "../../lib/sendgrid";
+
+export default async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    await sendEmail(
+      "info@granto.tech",
+      "Test Email",
+      "This is a test email from Next.js!"
+    );
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
